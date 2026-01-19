d8e0401750cad87836dda1b1e521986f5432ca24
M. Paz Jimenez <meripeich@gmail.com>
2026-01-19 12:27:12 +0100

    revisar funcion flecha

--- Tema 03/Clase 1/app.js
+++ Tema 03/Clase 1/app.js
@@ -52,6 +52,12 @@ console.log("Juegos tras push:", juegos);
 juegos.pop();
 console.log("Juegos tras pop:", juegos);
 
+// Arrays mixtos
+const mixto = ["Hola", 42, true, {juego: "Minecraft", pegi: 12}, [1, 2, 3]];
+console.log("Mixto:", mixto);
+console.log(`Elemento 0 (${mixto[0].constructor.name}): ${mixto[0]}`);
+console.log(typeof mixto[0], typeof mixto[1]);
+
 const alumno = {
     nombre: "Ana",
     curso: "1º DAW",
@@ -66,5 +72,55 @@ alumno.nombre = "Ana María";
 console.log("Nombre cambiado", alumno.nombre);
 console.log(alumno.edad.constructor.name); // devuelve el tipo (number)
 console.log(alumno.constructor.name); // devuelve el tipo (Object)
+console.log(alumno.toString());
+console.log(`Alumno:\nNombre - ${alumno.nombre}\nCurso - ${alumno.curso}`);
 
+const producto = {
+    nombre: "Producto X",
+    precio: 20.00,
+    stock: true,
+    toString: function() {
+        return `Producto: ${this.nombre} - Precio ${this.precio}$ - Stock: ${this.stock}`;
+    }
+};
 
+// console.log(`Producto: ${producto.nombre} - Precio: ${producto.precio}\$ - Stock: ${producto.stock}`);
+console.log(producto.toString());
+
+// Funciones
+const total = function (precio, unidades) {
+    return precio * unidades;
+};
+
+console.log("Total 9.99 * 3 = ", total(9.99, 3));
+console.log(total(9.99, 3).constructor.name);
+
+// Funciones flecha
+// const name = (params) => { cuerpo };
+const flecha = (precio, unidades) => precio * unidades;
+console.log("Total 12.50 * 7 = ", flecha(12.50, 7));
+
+// Crear un array de 3 objetos productos (nombre/precio)
+// y una función que devuelva el precio total sumado
+// (aquí puedes hacerlo simple: sumar 3 elementos a mano, sin bucles)
+const productos = [
+    {nombre: "Producto1", precio: 20.00}, 
+    {nombre: "Producto2", precio: 10.90}, 
+    {nombre: "Producto3", precio: 5.90}
+];
+
+const suma1 = function(precio1, precio2, precio3) {
+  return precio1, precio2, precio3;
+};
+// forma 1
+console.log(`Total compra cons suma1 = ${sumaproductos[0].precio, productos[1].precio, productos[2].precio}€`);
+
+// forma 2
+const suma2 = function(objeto1, objeto2, objeto3) {
+    return objeto1.precio + objeto2.precio + objeto3.precio;
+}
+console.log(`Total compra con suma2 = ${suma2[0], cesta[1], cesta[2]}€`);
+
+// forma 3
+const suma3 = (obj1, obj2, obj3) => obj1.precio + obj2.precio + obj3.precio;
+console.log(`Total compra con suma3 = ${obj1, obj2, obj3}€`);
